a52e8854c5da9a2f57c2518176fd8a0c76b3c83a
pengrui <55395901+Pengrui-Feng@users.noreply.github.com>
2019-11-04 11:31:33 -0500

    Update merge_realtime_turtle_td.py

--- merge_realtime_turtle_td.py
+++ merge_realtime_turtle_td.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 path1 = '/home/zdong/PENGRUI/get_original_data/'
 path2 = '/home/zdong/PENGRUI/data_process/'
 
+#convert time format
 df = pd.read_csv(path1 + 'tu102_ctd.csv')
 df['END_DATE'] = pd.to_datetime(df['END_DATE'])
 df.to_csv(path2 + 'tu102_ctd.csv')
@@ -20,7 +21,7 @@ df = pd.read_csv(path1 + 'tu102_gps.csv')
 df['D_DATE'] = pd.to_datetime(df['D_DATE'])
 df.to_csv(path2 + 'tu102_gps.csv')
 
-
+#read ctd and gps csv files
 dict_ctd = {}
 dict_ctd['PTT'] = []
 dict_ctd['argos_date'] = []
@@ -37,13 +38,7 @@ with open( path2 + 'tu102_ctd.csv','r') as csvfile:
         dict_ctd['TEMP_DBAR'].append(row['TEMP_DBAR'])
         dict_ctd['TEMP_VALS'].append(row['TEMP_VALS'])
         dict_ctd['lat'].append(row['lat'])
-        dict_ctd['lon'].append(row['lon'])
-        #print(row['PTT'], row['END_DATE'])         
-        #argos_date,argos_time = row['END_DATE'].split(' ')
-        #argo_date,argo_time = row['END_DATE'].split(' ')
-        #dict_ctd['argos_date'].append(argo_date)
-        #dict_ctd['argos_time'].append(argo_time)
-
+        dict_ctd['lon'].append(row['lon'])        
 
 dict_gps = {}
 dict_gps['PTT'] = []
@@ -58,15 +53,11 @@ with open( path2 + 'tu102_gps.csv','r') as csvfile:
         dict_gps['gps_date'].append(row['D_DATE'])
         dict_gps['LAT'].append(row['LAT'])
         dict_gps['LON'].append(row['LON'])
-        #print(row['PTT'], row['D_DATE'], row['LAT'], row['LON'])
-        #gps_date, gps_time = row['D_DATE'].split(' ')
-        #dict_gps['gps_date'].append(gps_date)
-        #dict_gps['gps_time'].append(gps_time)
 
 
- 
-#create a new dictionary
+#create a new final dictionary
 final_dict = {}
+final_dict['num'] = []
 final_dict['PTT'] = []
 final_dict['argos_date'] = []
 final_dict['TEMP_DBAR'] = []
@@ -76,41 +67,60 @@ final_dict['lon'] = []
 final_dict['gps_date'] = []
 final_dict['LAT'] = []
 final_dict['LON'] = []
+
+#compute min time diffirence to avoid multiple iterations
+print("min time compute: ")
+time_dict = {}
+for ctd_ptt,argos_date in tqdm(zip(dict_ctd['PTT'],dict_ctd['argos_date'])):
+    diff_time = []
+    for gps_ptt,gps_date in zip(dict_gps['PTT'],dict_gps['gps_date']):
+        if(ctd_ptt == gps_ptt and  abs((datetime.strptime(argos_date, "%Y-%m-%d %H:%M:%S") - datetime.strptime(gps_date, "%Y-%m-%d %H:%M:%S")).total_seconds()) < 3600*3):
+            timediff = abs((datetime.strptime(argos_date, "%Y-%m-%d %H:%M:%S") -  datetime.strptime(gps_date, "%Y-%m-%d %H:%M:%S")).total_seconds())
+            diff_time.append(timediff)
+    if ctd_ptt not in time_dict.keys():
+        time_dict[ctd_ptt] = {}
+    time_dict[ctd_ptt][argos_date]=diff_time
+#print(time_dict)
+
+
+print("merge csv: ")
 num=0
-#split cols to get one row for each depth
+tmp_ptt = dict_ctd['PTT'][0]
 for i,(ctd_ptt,argos_date) in enumerate(tqdm(zip(dict_ctd['PTT'],dict_ctd['argos_date']))):
+    if ctd_ptt != tmp_ptt:
+        num = 0
+        tmp_ptt = ctd_ptt
+#split cols to get one row for each depth
     for j,(gps_ptt,gps_date) in enumerate(zip(dict_gps['PTT'],dict_gps['gps_date'])):
-        if(ctd_ptt == gps_ptt and abs((datetime.strptime(argos_date,"%Y-%m-%d %H:%M:%S") - datetime.strptime(gps_date,"%Y-%m-%d %H:%M:%S")).total_seconds()) < 3600*3) :
+        if(ctd_ptt == gps_ptt and  abs((datetime.strptime(argos_date, "%Y-%m-%d %H:%M:%S") - datetime.strptime(gps_date, "%Y-%m-%d %H:%M:%S")).total_seconds()) < 3600*3):
+            timediff = abs((datetime.strptime(argos_date, "%Y-%m-%d %H:%M:%S") - datetime.strptime(gps_date, "%Y-%m-%d %H:%M:%S")).total_seconds())
+            min_timediff = min(time_dict[ctd_ptt][argos_date])
+            if timediff != min_timediff:
+                continue         
             list_dbar = dict_ctd['TEMP_DBAR'][i].split(',')
             list_vals = dict_ctd['TEMP_VALS'][i].split(',')
             num += 1
-            #print('split result',list_dbar,list_vals)
             for dbar, vals in zip(list_dbar,list_vals):
+                final_dict['num'].append(num)
                 final_dict['PTT'].append(ctd_ptt)
                 final_dict['argos_date'].append(dict_ctd['argos_date'][i])
-                #final_dict['argos_time'].append(dict_ctd['argos_time'][i])
                 final_dict['TEMP_DBAR'].append(dbar)
                 final_dict['TEMP_VALS'].append(vals)
                 final_dict['LAT'].append(dict_gps['LAT'][j])
                 final_dict['LON'].append(dict_gps['LON'][j])
                 final_dict['gps_date'].append(dict_gps['gps_date'][j])
-                #final_dict['gps_time'].append(dict_gps['gps_time'][j])
                 final_dict['lat'].append(dict_ctd['lat'][i])
                 final_dict['lon'].append(dict_ctd['lon'][i])
-                
-            #break
 
-#final_dict['PTT'],final_dict['END_DATE'] ,final_dict['TEMP_DBAR'],final_dict['TEMP_VALS'] ,final_dict['lat'],final_dict['lon'] ,final_dict['D_DATE'],final_dict['LAT'] ,final_dict['LON']
 
+print("write file: ")
 with open(path2 +'combined_td_gps.csv','w') as csvfile:
-    #fieldnames = ['PTT', 'argos_date', 'TEMP_DBAR', 'TEMP_VALS', 'lat', 'lon', 'D_DATE', 'LAT', 'LON']
-    fieldnames = ['PTT', 'argos_date', 'depth', 'temp', 'lat_gps', 'lon_gps', 'gps_date','lat_argos', 'lon_argos']
+    fieldnames = ['num','PTT', 'argos_date', 'depth', 'temp','lat_argos', 'lon_argos','gps_date','lat_gps', 'lon_gps']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
    
-    for ptt,argos_date,dbar,vals,lat,lon,gps_date,lat1,lon1 in tqdm(zip(final_dict['PTT'],final_dict['argos_date'],
-    final_dict['TEMP_DBAR'],final_dict['TEMP_VALS'] ,final_dict['lat'],
-    final_dict['lon'] ,final_dict['gps_date'],final_dict['LAT'] ,final_dict['LON'])) :
-        writer.writerow({'PTT': ptt, 'argos_date':argos_date, 'depth':dbar, 'temp':vals, 'lat_gps':lat1, 'lon_gps':lon1, 'gps_date':gps_date, 'lat_argos':lat, 'lon_argos':lon})
-  
+    for num,ptt,argos_date,dbar,vals,lat1,lon1,gps_date,lat,lon in tqdm(zip(final_dict['num'],final_dict['PTT'],final_dict['argos_date'],
+    final_dict['TEMP_DBAR'],final_dict['TEMP_VALS'] ,final_dict['LAT'] ,final_dict['LON'] ,final_dict['gps_date'],final_dict['lat'],
+    final_dict['lon'])) :
+        writer.writerow({'num':num,'PTT': ptt, 'argos_date':argos_date, 'depth':dbar, 'temp':vals, 'lat_argos':lat, 'lon_argos':lon, 'gps_date':gps_date,'lat_gps':lat1, 'lon_gps':lon1 })
 
