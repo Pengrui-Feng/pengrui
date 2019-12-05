134b7c9e4df8ded919b858d12b55d961b47214d1
pengrui <55395901+Pengrui-Feng@users.noreply.github.com>
2019-12-05 11:37:28 -0500

    Update summary_turtles.py

--- summary_turtles.py
+++ summary_turtles.py
@@ -2,20 +2,22 @@
 """
 Created on Tue Oct 22 09:37:53 2019
 
-Summary specified data by pandas
+summary specified data by pandas
 
 @author: pengrui
 """
-
+from datetime import datetime
 import pandas as pd
 import numpy as np
 import csv
 
+db= 'tu73' #tu73,tu74,tu94,tu98,tu99,tu102
+path1 = '/home/zdong/PENGRUI/merge/'
+path2 = '/home/zdong/PENGRUI/summary/'
 #input csv file,
-df = pd.read_csv("combined_td_gps.csv")#,usecols=['PTT','argos_date','argos_time','lat_argos','lon_argos']
-df['argos_date'] = pd.to_datetime(df['argos_date'])
+df = pd.read_csv(path1+db+"_merge_td_gps.csv")# modify the name of file
+df['argos_date'] = pd.to_datetime(df['END_DATE'])
 #a = df.set_index('PTT')
-
 
 #Count the number of turtles and times each turtle transmits data
 '''
@@ -25,33 +27,34 @@ nums_ptt.count()
 '''
 #date = pd.Series(df['argos_date'])
 #nums_date = date.value_counts()
-nums_ptt = df['PTT'].groupby(df['PTT']).count()
+count_ptt = df['PTT'].groupby(df['PTT']).count()
 
 #date
+df['argos_date']=df['argos_date']
 grouped1 = df['argos_date'].groupby(df['PTT'])
 date_max = grouped1.max()
 date_min = grouped1.min()
 delta_date = grouped1.max() - grouped1.min()
 
 #lat
-grouped2 = df['lat_argos'].groupby(df['PTT'])
+grouped2 = df['lat'].groupby(df['PTT'])
 lat_max = grouped2.max()
 lat_min = grouped2.min()
 delta_lat = grouped2.max() - grouped2.min()
 
 #lon
-grouped3 = df['lon_argos'].groupby(df['PTT'])
+grouped3 = df['lon'].groupby(df['PTT'])
 lon_max = grouped3.max()
 lon_min = grouped3.min()
 delta_lon = grouped3.max() - grouped3.min()
-
+'''
 #gps
-gps = df['lat_gps'].groupby(df['PTT']).mean()
+gps = df['LAT'].groupby(df['PTT']).mean()
 r=gps.notnull()
-
+'''
 #Create a DataFrame with multiple Series
 c = pd.DataFrame()
-c['nums_ptt']=pd.Series(nums_ptt)
+c['count_ptt']=pd.Series(count_ptt)
 c['start_date']=pd.Series(date_min)
 c['end_date']=pd.Series(date_max)
 c['length_of_track']=pd.Series(delta_date)
@@ -61,7 +64,6 @@ c['lon_min']=pd.Series(lon_min)
 c['lon_max']=pd.Series(lon_max)
 c['delta_lat']=pd.Series(delta_lat)
 c['delta_lon']=pd.Series(delta_lon)
-c['if_with_gps']=pd.Series(r)
+#c['if_with_gps']=pd.Series(r)
 
-
-c.to_csv('Summary.csv')
+c.to_csv(path2+db+'_Summary.csv')
