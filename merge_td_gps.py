08c1d2e9f4290cc62e5975c3586882efaf5bf6e2
pengrui <55395901+Pengrui-Feng@users.noreply.github.com>
2020-05-14 17:06:38 -0400

    Update and rename combine_td_gps.py to merge_td_gps.py

--- merge_td_gps.py
+++ merge_td_gps.py
@@ -8,21 +8,24 @@ import pandas as pd
 from datetime import datetime,timedelta
 import csv
 from tqdm import tqdm
+######hardcode########
 db= 'tu109' #tu55,63,67,68,69,73,74,75,80,84,86,87,88,91,92,94,98,99,102,109
-path1 = '/content/drive/My Drive/PENGRUI/get_original_data/'
-path2 = '/content/drive/My Drive//PENGRUI/data_process_test/'
-hour=3600*3 #match ctd and gps in 3 hours
+path1 = '/content/drive/My Drive/PENGRUI/get_original_data/' 
+path2 = '/content/drive/My Drive//PENGRUI/merge_split/'
+path3 = '/content/drive/My Drive/PENGRUI/merge_nosplit/'
+hour=3 #match ctd and gps in 3 hours
+
 #convert time format
-df = pd.read_csv(path1 + db+'_ctd.csv')
+df = pd.read_csv(path1 + db+'_ctd.csv') 
 df['END_DATE'] = pd.to_datetime(df['END_DATE'])
 df.rename(columns = {"LAT": "lat",'LON':'lon'},inplace=True) #tu73,tu74
 df=df.dropna(subset=['lat'])
-df.to_csv(path2 + db+'_ctd.csv')
+df.to_csv(path1 + db+'_ctd.csv')
 
 df = pd.read_csv(path1 + db+'_gps.csv')
 df['D_DATE'] = pd.to_datetime(df['D_DATE'])
 df=df.dropna(subset=['PTT'])
-df.to_csv(path2 + db+'_gps.csv')
+df.to_csv(path1 + db+'_gps.csv')
 
 #read ctd and gps csv files
 dict_ctd = {}
@@ -79,7 +82,7 @@ time_dict = {}
 for ctd_ptt,argos_date in tqdm(zip(dict_ctd['PTT'],dict_ctd['argos_date'])):
     diff_time = []
     for gps_ptt,gps_date in zip(dict_gps['PTT'],dict_gps['gps_date']):
-        if(ctd_ptt == gps_ptt and  abs((datetime.strptime(argos_date, "%Y-%m-%d %H:%M:%S") - datetime.strptime(gps_date, "%Y-%m-%d %H:%M:%S")).total_seconds()) < hour):
+        if(ctd_ptt == gps_ptt and  abs((datetime.strptime(argos_date, "%Y-%m-%d %H:%M:%S") - datetime.strptime(gps_date, "%Y-%m-%d %H:%M:%S")).total_seconds()) < hour*3600):
             timediff = abs((datetime.strptime(argos_date, "%Y-%m-%d %H:%M:%S") -  datetime.strptime(gps_date, "%Y-%m-%d %H:%M:%S")).total_seconds())
             diff_time.append(timediff)
     if ctd_ptt not in time_dict.keys():
@@ -96,7 +99,7 @@ for i,(ctd_ptt,argos_date) in tqdm(enumerate(zip(dict_ctd['PTT'],dict_ctd['argos
         tmp_ptt = ctd_ptt
 #split cols to get one row for each depth
     for j,(gps_ptt,gps_date) in enumerate(zip(dict_gps['PTT'],dict_gps['gps_date'])):
-        if(ctd_ptt == gps_ptt and  abs((datetime.strptime(argos_date, "%Y-%m-%d %H:%M:%S") - datetime.strptime(gps_date, "%Y-%m-%d %H:%M:%S")).total_seconds()) < hour):
+        if(ctd_ptt == gps_ptt and  abs((datetime.strptime(argos_date, "%Y-%m-%d %H:%M:%S") - datetime.strptime(gps_date, "%Y-%m-%d %H:%M:%S")).total_seconds()) < hour*3600):
             timediff = abs((datetime.strptime(argos_date, "%Y-%m-%d %H:%M:%S") - datetime.strptime(gps_date, "%Y-%m-%d %H:%M:%S")).total_seconds())
             min_timediff = min(time_dict[ctd_ptt][argos_date])
             if timediff != min_timediff:
@@ -118,7 +121,7 @@ for i,(ctd_ptt,argos_date) in tqdm(enumerate(zip(dict_ctd['PTT'],dict_ctd['argos
             break
 
 print("\nwriting file: ")
-with open('/content/drive/My Drive/PENGRUI/merge_split/'+db+'_merge_split.csv','w') as csvfile:
+with open(path2+db+'_merge_split.csv','w') as csvfile:
     fieldnames = ['dive_num','PTT', 'argos_date', 'depth', 'temp','lat_argos', 'lon_argos','gps_date','lat_gps', 'lon_gps']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
@@ -129,11 +132,11 @@ with open('/content/drive/My Drive/PENGRUI/merge_split/'+db+'_merge_split.csv','
         writer.writerow({'dive_num':num,'PTT': ptt, 'argos_date':argos_date, 'depth':dbar, 'temp':vals, 'lat_argos':lat, 'lon_argos':lon, 'gps_date':gps_date,'lat_gps':lat1, 'lon_gps':lon1 })
 
 ###convert to nosplit
-df=pd.read_csv('/content/drive/My Drive/PENGRUI/merge_split/'+db+'_merge_split.csv')
+df=pd.read_csv(path2+db+'_merge_split.csv')
 df['depth'] = df['depth'].astype('str')
 df['temp']= df['temp'].astype('str')
 df=df.groupby(['dive_num','PTT','argos_date','lat_argos','lon_argos','gps_date','lat_gps','lon_gps'])['depth','temp'].agg(lambda x:x.str.cat(sep=','))
-df.to_csv('/content/drive/My Drive/PENGRUI/merge_nosplit/'+db+'_merge_td_gps.csv')
+df.to_csv(path3+db+'_merge_td_gps.csv')
 df=pd.read_csv('/content/drive/My Drive/PENGRUI/merge_nosplit/'+db+'_merge_td_gps.csv')
 df=df[['PTT','dive_num','gps_date','lat_gps','lon_gps','depth','temp','argos_date','lat_argos','lon_argos']]
 df.to_csv('/content/drive/My Drive/PENGRUI/merge_nosplit/'+db+'_merge_td_gps.csv')
